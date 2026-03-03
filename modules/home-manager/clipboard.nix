89bfbc66915a22fa2b61e2729d4aa1ab1e80447b
HerrMartinsen <43820921+HerrMartinsen@users.noreply.github.com>
2026-03-03 12:44:24 +0100

    clipse++

--- modules/home-manager/clipboard.nix
+++ modules/home-manager/clipboard.nix
@@ -1,5 +1,12 @@
-{ ... }:
-
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  json = pkgs.formats.json { };
+in
 {
   services.clipse = {
     enable = true;
@@ -7,4 +14,21 @@
     historySize = 200;
     imageDisplay.type = "kitty";
   };
+  xdg.configFile."clipse/config.json".source = lib.mkForce (
+    json.generate "clipse-config" {
+      allowDuplicates = config.services.clipse.allowDuplicates;
+      historyFile = "clipboard_history.json";
+      maxHistory = config.services.clipse.historySize;
+      logFile = "clipse.log";
+      themeFile = "custom_theme.json";
+      tempDir = "tmp_files";
+
+      keyBindings = config.services.clipse.keyBindings;
+      imageDisplay = config.services.clipse.imageDisplay;
+
+      excludedApps = [
+        "Bitwarden"
+      ];
+    }
+  );
 }
